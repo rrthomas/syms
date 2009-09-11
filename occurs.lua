dd51298491f87c65919e89082837937a595971de
Reuben Thomas <rrt@sc3d.org>
2009-09-11 16:47:20 +0100

    Remove version history from source file.

--- occurs.lua
+++ occurs.lua
@@ -1,4 +1,4 @@
-#! /usr/bin/env lua
+#!/usr/bin/env lua
 prog = {
   name = "occurs",
   banner = "occurs 0.83 (25 Sep 2008) by Reuben Thomas <rrt@sc3d.org>",
@@ -86,40 +86,3 @@ if getopt.opt.right  then
 end
 -- FIXME: checkRegex (symbolPat, leftDel, rightDel)
 io.processFiles (occurs)
-
-
--- Changelog
--- 25sep08  0.83 Use io.lines and list.elems iterators
--- 16nov07  0.82 Change one last table.n to #table
--- 24dec06  0.81 Fix remaining space before argument in documentation
--- 20oct06  0.8  Fix documentation: command-line arguments always need
---               space before; change "word" to "symbol"
---               Change -byfreqs to -sort to allow no sorting; make
---               that the default
---               Change -nofreqs to -nocount, and if used, don't
---               display total number of symbols
---               Anonymised one-use local functions
--- 08oct06  0.75 table.getn --> #
--- 29mar06  0.74 Updated to match stdlib
--- 05may05  0.73 Updated to match stdlib
--- 15jan05  0.59 (Updated C version)
--- 29jan04  0.72 Don't die with usage if no arguments given
--- 26jan04  0.71 Updated to match stdlib
--- 21oct03  0.70 Updated to match stdlib
---               Don't die with usage when some options are given but
---               no files
--- 18oct03  0.69 Updated to match stdlib
--- 12sep03  0.68 Update for Lua 5
--- 12aug02  0.67 Now need to print usage message explicitly if no args
--- 22jun02  0.66 Updated to match stdlib
--- 18jan02  0.65 Code rearranged
--- 03jan02  0.64 Updated to match stdlib
--- 11aug01  0.63
--- 27jul01  0.62
--- 17jul01  0.61
--- 02jul01  0.6  Lua version
--- 30jun01- 0.58 Eric Hutton's (bookman@rmplc.co.uk) comments on the
--- 25nov97  0.4  BASIC version caused me to translate it to C and
---               improve it.
--- 23nov97- 0.33 BBC BASIC V version
--- 25jan92  0.1
