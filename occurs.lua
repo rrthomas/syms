6298d8342554a26ba525d0316489c09c078325a8
Reuben Thomas <rrt@sc3d.org>
2011-09-27 13:47:18 +0100

    FIXME(occurs.lua): use POSIX regexs so syntax can be checked

--- occurs.lua
+++ occurs.lua
@@ -1,37 +1,37 @@
 #!/usr/bin/env lua
 prog = {
   name = "occurs",
-  banner = "occurs 0.84 (21 May 2011) by Reuben Thomas <rrt@sc3d.org>",
+  banner = "occurs 0.85 (27 Sep 2011) by Reuben Thomas <rrt@sc3d.org>",
   purpose = "Count the occurrences of each symbol in a file",
-  notes = "The default symbol type is words (-s \"%a+\"); other " ..
-    "useful settings include:\n\n" ..
-    "  non-white-space characters: -s \"%S+\"\n" ..
-    "  alphanumerics and underscores: -s \"[%w_]+\"\n" ..
-    "  XML tags: -s \"[a-zA-Z_:][a-zA-Z_:.0-9-]*\" -l \"\<\" -r \"[%s\\>]\""
+  notes = "The default symbol type is words (-s \"[[:alpha:]]+\"); other useful settings\n" ..
+    "include:\n\n" ..
+    "  non-white-space characters: -s \"[^[:space:]]+\"\n" ..
+    "  alphanumerics and underscores: -s \"[[:alnum:]_]+\"\n" ..
+    "  XML tags: -s \"[a-zA-Z_:][a-zA-Z_:.0-9-]*\" -l \"\<\" -r \"[[:space:]>]\""
 }
 
 -- FIXME: rewrite in Python
--- FIXME: bullet-proof by writing checkRegex
 
 require "std"
-
+require "rex_posix"
 
 -- Process a file
+local pattern -- regexp, defined below
 function occurs (file, number)
   local symbol, freq = {}, {}
   for line in io.lines () do
-    string.gsub (line, leftDel .. "(" .. symbolPat .. ")" .. rightDel,
-                 function (s)
-                   if getopt.opt.lower then
-                     s = string.lower (s)
-                   end
-                   if not freq[s] then
-                     table.insert (symbol, s)
-                     freq[s] = 1
-                   else
-                     freq[s] = freq[s] + 1
-                   end
-                 end)
+    rex_posix.gsub (line, pattern,
+                    function (s)
+                      if getopt.opt.lower then
+                        s = string.lower (s)
+                      end
+                      if not freq[s] then
+                        table.insert (symbol, s)
+                        freq[s] = 1
+                      else
+                        freq[s] = freq[s] + 1
+                      end
+                  end)
   end
   if getopt.opt.sort == "lexical" then
     table.sort (symbol)
@@ -58,31 +58,28 @@ end
 
 -- Command-line options
 options = {
-  Option {{"sort", "S"},
-    "sort by given method (one of `lexical', `frequency')",
-    "Req", "METHOD"},
+  Option {{"sort", "S"}, "sort by given method (one of `lexical', `frequency')", "Req", "METHOD"},
   Option {{"nocount", "n"}, "don't show the frequencies or total"},
-  Option {{"symbol", "s"}, "symbols are given by REGEXP",
-    "Req", "REGEXP"},
-  Option {{"left", "l"}, "symbols must be preceded by REGEXP",
-    "Req", "REGEXP"},
-  Option {{"right", "r"}, "symbols must be followed by REGEXP",
-    "Req", "REGEXP"},
+  Option {{"symbol", "s"}, "symbols are given by REGEXP", "Req", "REGEXP"},
+  Option {{"left", "l"}, "symbols must be preceded by REGEXP", "Req", "REGEXP"},
+  Option {{"right", "r"}, "symbols must be followed by REGEXP", "Req", "REGEXP"},
   Option {{"lower", "L"}, "symbols are converted to lower case"},
 }
 
--- Main routine
+
+-- Parse command-line args
 os.setlocale ("")
 getopt.processArgs ()
-symbolPat, leftDel, rightDel = "%a+", "", ""
-if getopt.opt.symbol then
-  symbolPat = getopt.opt.symbol
+local symbolPat = getopt.opt.symbol or "[[:alpha:]]+"
+local leftDel = getopt.opt.left or ""
+local rightDel = getopt.opt.right or ""
+
+-- Compile symbol-matching regexp
+local err
+pattern, err = rex_posix.new (leftDel .. "(" .. symbolPat .. ")" .. rightDel)
+if err then
+  io.stderr:write (prog.name .. ": " .. err)
+  os.exit (1)
 end
-if getopt.opt.left then
-  leftDel = getopt.opt.left
-end
-if getopt.opt.right then
-  rightDel = getopt.opt.right
-end
--- FIXME: checkRegex (symbolPat, leftDel, rightDel)
+
 io.processFiles (occurs)
