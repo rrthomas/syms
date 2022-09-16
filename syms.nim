d178e72cad7d657091785e99125abb1de410fb4a
Reuben Thomas <rrt@sc3d.org>
2022-09-16 23:40:54 +0100

    Use Unicode character classes in Python and Nim
    (Both use PCRE.)
    
    In Python, this uses proper Unicode classes. In Nim there doesn’t seem to be
    a way to do that; it only matches ASCII.

--- syms.nim
+++ syms.nim
@@ -1,7 +1,6 @@
 import os
 import system
 import argparse
-#import posix
 import re
 import logging
 import strformat
@@ -19,7 +18,7 @@ proc die(s: string) =
 # Command-line arguments
 var p = newParser(progName):
   help("List symbols in input.")
-  option("-s", "--symbol", default = "[\\w\\d_]+",
+  option("-s", "--symbol", default = "\\p{L}+",
          help = "symbols are given by the regexp SYMBOL")
   flag("-V", "--version",
        help = "show program's version number and exit")
@@ -32,7 +31,7 @@ try:
   if opts.help:
     echo """
 
-The default symbol type is words (-s "[\w\d_]+"); other useful settings
+The default symbol type is words (-s "\p{L}+"); other useful settings
 include:
 
   non-white-space characters: -s "\S+"
@@ -40,14 +39,10 @@ include:
   XML tags: -s "<([a-zA-Z_:][a-zA-Z_:.0-9-]*)[\s>]""""
     quit(0)
   if opts.version:
-    echo &"{progName} 0.11 (16 Sep 2022) by Reuben Thomas <rrt@sc3d.org>"
+    echo &"{progName} 0.12 (16 Sep 2022) by Reuben Thomas <rrt@sc3d.org>"
     quit(0)
 
-  # Set locale
-  #discard setlocale(LC_ALL, "")
-
   # Compile symbol-matching regexp
-  # FIXME: use locale-sensitive regexs
   let pattern = re(opts.symbol)
 
   # Process input
--- syms.py
+++ syms.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 import argparse
 import locale
-import re
+import regex
 import fileinput
 
 # Command-line arguments
 parser = argparse.ArgumentParser(description='List symbols in input.',
                                  epilog='''
-The default symbol type is words (-s "[\w\d_]+"); other useful settings
+The default symbol type is words (-s "\p{L}+"); other useful settings
 include:
 
   non-white-space characters: -s "\S+"
@@ -18,10 +17,10 @@ include:
   XML tags: -s "<([a-zA-Z_:][a-zA-Z_:.0-9-]*)[\s>]"
 ''',
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument('-s', '--symbol', metavar='REGEXP', type=os.fsencode, default=b'[\w\d_]+',
+parser.add_argument('-s', '--symbol', metavar='REGEXP', default=r'\p{L}+',
                     help='symbols are given by REGEXP')
 parser.add_argument('-V', '--version', action='version',
-                    version='%(prog)s 0.9 (14 Jan 2012) by Reuben Thomas <rrt@sc3d.org>')
+                    version='%(prog)s 0.91 (16 Sep 2022) by Reuben Thomas <rrt@sc3d.org>')
 parser.add_argument('file', metavar='FILE', nargs='*')
 
 args = parser.parse_args()
@@ -31,11 +30,11 @@ locale.setlocale(locale.LC_ALL, '')
 
 # Compile symbol-matching regexp
 try:
-    pattern = re.compile(args.symbol, re.LOCALE)
-except re.error as err:
+    pattern = regex.compile(args.symbol)
+except regex.error as err:
     parser.error(err.args[0])
 
 # Process input
-for line in fileinput.input(mode='rb', files=args.file or ['-']):
+for line in fileinput.input(files=args.file or ['-']):
     for s in pattern.findall(line):
-        sys.stdout.buffer.write(s + b"\n")
+        print(s)
