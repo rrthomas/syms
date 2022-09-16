ffa754d29af2b4dffe03fab7dce887897205c44b
Reuben Thomas <rrt@sc3d.org>
2022-09-17 00:14:26 +0100

    Rust: run 'cargo fmt'

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+use clap::{App, Arg, Error};
+use regex::Regex;
 use std::fs::File;
 use std::io::{self, BufRead};
-use clap::{App, Arg, Error};
-use regex::{Regex};
 
-fn match_lines(pattern: &Regex, fp: impl BufRead)
-{
+fn match_lines(pattern: &Regex, fp: impl BufRead) {
     for line in fp.lines() {
         if let Ok(ip) = line {
             for mat in pattern.find_iter(&ip) {
@@ -14,29 +13,30 @@ fn match_lines(pattern: &Regex, fp: impl BufRead)
     }
 }
 
-fn main()
-{
+fn main() {
     // Command-line arguments
     let matches = App::new("syms")
         .version("0.11")
         .about("List symbols in input.")
         .author("Reuben Thomas")
-        .arg(Arg::with_name("REGEXP")
-             .short("s")
-             .long("symbol")
-             .takes_value(true)
-             .default_value("\\p{L}+")
-             .help("symbols are given by REGEXP"))
-        .arg(Arg::with_name("FILE")
-             .default_value("-")
-             .multiple(true))
-        .after_help("The default symbol type is words (-s \"\\p{L}+\"); other useful settings
+        .arg(
+            Arg::with_name("REGEXP")
+                .short("s")
+                .long("symbol")
+                .takes_value(true)
+                .default_value("\\p{L}+")
+                .help("symbols are given by REGEXP"),
+        )
+        .arg(Arg::with_name("FILE").default_value("-").multiple(true))
+        .after_help(
+            "The default symbol type is words (-s \"\\p{L}+\"); other useful settings
 include:
 
   non-white-space characters: -s \"\\S+\"
   alphanumerics and underscores: -s \"\\w+\"
   XML tags: -s \"<([a-zA-Z_:][a-zA-Z_:.0-9-]*)[\\s>]\"
-")
+",
+        )
         .get_matches();
     let symbol = matches.value_of("REGEXP").unwrap();
 
@@ -44,12 +44,11 @@ include:
     let pattern = match Regex::new(symbol) {
         Ok(regex) => regex,
         Err(error) => match error {
-            regex::Error::Syntax(string) =>
-                Error::exit(&Error {
-                    message: string,
-                    kind: clap::ErrorKind::InvalidValue,
-                    info: None
-                }),
+            regex::Error::Syntax(string) => Error::exit(&Error {
+                message: string,
+                kind: clap::ErrorKind::InvalidValue,
+                info: None,
+            }),
             error => panic!("{}", error),
         },
     };
@@ -64,7 +63,7 @@ include:
                 Err(e) => Error::exit(&Error {
                     message: e.to_string(),
                     kind: clap::ErrorKind::Io,
-                    info: None
+                    info: None,
                 }),
             };
             match_lines(&pattern, fp);
